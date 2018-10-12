89e1655b711f4bb6e95e42e774840566c550324e
Daniel Werner <daniel.werner@smail.th-koeln.de>
2018-10-12 20:42:42 +0200

    Added automatic creation of default admin

--- Backend/setupDB.js
+++ Backend/setupDB.js
@@ -4,3 +4,5 @@ db.createCollection("pages");
 db.createCollection("cmLoggeduser")
 db.createCollection("cmKnownUser")
 db.createCollection("permissions")
+db.permissions.insert({"role" : "admin", "permissionContent" : "crud", "permissionPages" : "crud", "admin" : true })
+db.cmKnownUser.insert({"name" : "Test Admin", "email" : "admin@admin.com", "role" : "admin", "pw" : "f89edc688a7886e8288fa20ce1baaacb421bf047bdd8fe1d5f0c6c68d7930fd6" })
